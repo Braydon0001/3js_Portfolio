ade5ab56a8bacd2e56936f5e77a9bb3ddd728804
Braydon Padayachee <bpadayachee@insightconsulting.co.za>
2023-06-12 23:52:41 +0200

    laptop responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
 
   return (
     <div id="about" className="w-fit mx-6 xs:mx-auto my-[80px] relative">
-      <motion.div variants={textVariant()}>
+      <motion.div variants={textVariant()} className="xl:mx-0 xs:mx-[60px]">
         <p className={`${styles.sectionSubText} text-slate-100 w-fit`}>
           Introduction
         </p>
@@ -23,7 +23,7 @@ const About = () => {
       </motion.div>
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-slate-100 text-[18px] max-w-3xl leading-[30px]"
+        className="xl:mx-0 xs:mx-[60px] mt-4 text-slate-100 text-[18px] max-w-3xl leading-[30px]"
       >
         I'm a Senior UI/UX designer and Intermediate full stack developer from
         South Africa. With a Higher Certificate in Information Systems &
@@ -33,14 +33,14 @@ const About = () => {
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10 w-fit mr-5">
-        <div className="w-[1600px] flex justify-between relative">
-          <div>
-            <ul className="list-none flex flex-col gap-1">
+        <div className="xl:w-[1560px] flex flex-wrap justify-between relative">
+          <div className="w-full xl:w-[800px] flex justify-between relative ">
+            <ul className="list-none flex flex-wrap flex-row xl:flex-col  justify-center gap-x-10 xl:px-0 px-5 xl:gap-1">
               {services.map((service, index) => (
                 <li
                   onClick={() => setSkill(index)}
                   data-content={service.title}
-                  className={`lg:text-[74px] sm:text-[60px] xs:text-[50px] text-[40px] font-bold cursor-pointer text-transparent stroke-white stroke-1 listItems relative ${
+                  className={`lg:text-[74px] text-center xl:text-left w-[45%] xl:w-full sm:text-[60px] xs:text-[50px] text-[40px] font-bold cursor-pointer text-transparent stroke-white stroke-1 listItems relative ${
                     skill == index && "active"
                   }`}
                 >
@@ -50,27 +50,27 @@ const About = () => {
             </ul>
           </div>
 
-          <>
-            <div className="glass w-[350px] rounded-lg absolute right-[60px]  p-5 top-[90px]">
+          <div className="w-full xl:w-[760px] flex justify-between relative ">
+            <div className="glass w-[350px] rounded-lg absolute right-[120px] xl:right-[60px]  p-5 top-[40px] xl:top-[90px]">
               <p className="text-[21px]">{services[skill].skillGroups[2]}</p>
             </div>
 
-            <div className="glass w-[350px] rounded-lg absolute  right-[80px]  z-10  p-5 top-[330px]">
+            <div className="glass w-[350px] rounded-lg absolute right-[140px] xl:right-[80px]  z-10  p-5 top-[150px] xl:top-[330px]">
               <p className="text-[21px]">{services[skill].skillGroups[3]}</p>
             </div>
 
-            <div className="glass w-[350px] rounded-lg absolute  z-10 p-5 top-[40px] right-[610px]">
+            <div className="glass w-[350px] rounded-lg absolute  z-10 p-5 top-[40px] left-[140px] xl:left-auto  xl:right-[610px]">
               <p className="text-[21px]">{services[skill].skillGroups[0]}</p>
             </div>
 
-            <div className="glass w-[350px] rounded-lg absolute  p-5 top-[290px] right-[630px]">
+            <div className="glass w-[350px] rounded-lg absolute  p-5 top-[150px] xl:top-[290px] xl:left-auto left-[120px]  xl:right-[630px]">
               <p className="text-[21px]">{services[skill].skillGroups[1]}</p>
             </div>
 
-            <div className="w-2/3">
+            <div className="w-full mt-10 xl:mt-0 xl:w-2/3">
               <ReactModel />
             </div>
-          </>
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,6 +18,7 @@ module.exports = {
       },
       screens: {
         xs: "501px",
+        xl: "1600px",
       },
       backgroundImage: {
         "hero-pattern": "url('/src/assets/bg4.svg')",
